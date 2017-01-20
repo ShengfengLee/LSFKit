96db122f7a309224b969f4166ebe8064421a9edf
李胜锋 <809243345@qq.com>
2017-01-20 14:32:07 +0800

    修改podspec

---
+++ LSFKit/LSFKit.m
@@ -0,0 +1,13 @@
+//
+//  LSFKit.h
+//  LSFKit
+//
+//  Created by 李胜锋 on 2017/1/20.
+//  Copyright © 2017年 ShengfengLee. All rights reserved.
+//
+
+#ifndef LSFKit_h
+#define LSFKit_h
+
+
+#endif /* LSFKit_h */
